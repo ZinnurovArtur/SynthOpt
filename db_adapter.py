c011990137b671cbbf0fe8d05000a20a28db55a3
Arthur zinnurov <zinnurov_2012@mail.ru>
2025-07-22 10:50:05 +0100

    fixing some issues with db_adapter

--- db_adapter.py
+++ db_adapter.py
@@ -1,18 +1,30 @@
 import pandas as pd
 import warnings
 from sqlalchemy import create_engine, text
-import sqlalchemy.engine
 from trino.auth import OAuth2Authentication
 
-engine = 
-
 class TrinoDBAdapter:
-    def __init__(self,username,host,port=443,verify=False)
+    def __init__(self, username, host, port=443, verify=False):
         self.engine = create_engine(
-    "trino://{your username here}@trino.feasibility.sail.pk.serp.ac.uk:443",
-    connect_args={
-        "auth": OAuth2Authentication(),
-        "http_scheme": "https",
-        "verify": False
-    }
-)
+            f"trino://{username}@{host}:{port}",
+            connect_args={
+                "auth": OAuth2Authentication(),
+                "http_scheme": "https",
+                "verify": verify
+            }
+        )
+
+
+    def query(self, sql):
+        with self.engine.connect() as connection:
+            return pd.read_sql(sql, connection)
+    
+    def get_cursor(self):
+        conn = self.engine.connect()
+        cursor = conn.connection.cursor()
+        return cursor, conn
+
+if __name__ == "__main__":
+    adapter = TrinoDBAdapter(username="username",host="trino.feasibility.sail.pk.serp.ac.uk")
+    df = adapter.query("SELECT ss.* FROM iceberg.pedw.superspell_20231127 ss")
+    print(df.head())
