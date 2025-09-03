3f332830ea6cdb98efd4bab0bcc23e87574a9abc
Arthur zinnurov <zinnurov_2012@mail.ru>
2025-09-03 10:34:34 +0100

    Adding table name to the main wdsd file

--- dataset_helpers/wdsd.py
+++ dataset_helpers/wdsd.py
@@ -1,20 +1,22 @@
 from synthopt.process.db_adapter import TrinoDBAdapter
 
 
-SELECT_AR_RESIDENCY_GPREG_20250602 = "SELECT * FROM iceberg.wdsd.per_residence_gpreg_20250602"
-SELECT_SINGLE_CLEAN_AR_PERS = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_ar_pers"
-SELECT_CLEAN_GEOCHAR_LSOA2001 = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_geo_char_lsoa2001"
-SELECT_CLEAN_GEOCHAR_LSOA2011 = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_geo_char_lsoa2011"
-SELECT_SINGLE_CLEAN_GEO_RALF = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_geo_ralf"
-SELECT_SINGLE_CLEAN_GEO_RALF_LSOA2001 = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_geo_ralf_lsoa2001"
-SELECT_SINGLE_CLEAN_GEO_RALF_LSOA2011 = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_geo_ralf_lsoa2011"
-SELECT_SINGLE_CLEAN_GEO_WALES = "SELECT * FROM iceberg.wdsd.wdsd_single_clean_geo_wales"
 
+WDSD_TABLES = {
+    'per_residence_gpreg_20250602': 'iceberg.wdsd.per_residence_gpreg_20250602',
+    'wdsd_single_clean_ar_pers': 'iceberg.wdsd.wdsd_single_clean_ar_pers',
+    'wdsd_single_clean_geo_char_lsoa2001': 'iceberg.wdsd.wdsd_single_clean_geo_char_lsoa2001',
+    'wdsd_single_clean_geo_char_lsoa2011': 'iceberg.wdsd.wdsd_single_clean_geo_char_lsoa2011',
+    'wdsd_single_clean_geo_ralf': 'iceberg.wdsd.wdsd_single_clean_geo_ralf',
+    'wdsd_single_clean_geo_ralf_lsoa2001': 'iceberg.wdsd.wdsd_single_clean_geo_ralf_lsoa2001',
+    'wdsd_single_clean_geo_ralf_lsoa2011': 'iceberg.wdsd.wdsd_single_clean_geo_ralf_lsoa2011',
+    'wdsd_single_clean_geo_wales': 'iceberg.wdsd.wdsd_single_clean_geo_wales'
+}
 
 def get_tables(adapter: TrinoDBAdapter, limit: int = 10000, offset: int = 0,selectQuery: str = ""):
     cursor = adapter.get_cursor()
     query = f'''
-        {selectQuery}
+        "SELECT * FROM {selectQuery}
         OFFSET {offset} LIMIT {limit}
     '''
     cursor.execute(query)
